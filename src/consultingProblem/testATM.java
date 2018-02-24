91a88d899cff7fa44163b80d278b4c5febecd5ab
Unknown <20harmet@elmbrookstudents.org>
2018-02-24 17:28:22 -0600

    Added FileIO

--- src/consultingProblem/testATM.java
+++ src/consultingProblem/testATM.java
@@ -5,31 +5,34 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class testATM {
 	public static void main(String[] args) throws IOException {
-		File file = new File("C:/Users/Thomas/workspace/SafeNetProblem/src/consultingProblem/file.txt");
-		Scanner input = new Scanner(new FileInputStream(file));
-		PrintWriter output = new PrintWriter(new FileOutputStream(file));
+		File inputFile = new File("inputFile.txt");
+		//PrintWriter output2 = new PrintWriter(new FileOutputStream(inputFile));
+		//output2.close();
+		File outputFile = new File("outputFile.txt");
+		Scanner input = new Scanner(new FileInputStream(inputFile));
 		boolean tru = true;
+		ArrayList<String> out = new ArrayList<String>();
 		ATM atm = new ATM();
 		while (tru && input.hasNextLine()) {
-			output.println("R - Restocks the cash machine");
-			output.println("W- Withdraws that amount from the cash machine (e.g. W $145");
-			output.println(
-					"O- Displays the number of bills in that denomination present in the cash machine (e.g. I $20 $10 $1)");
-			output.println("Q - Quits the application");
+			/*out.add("R - Restocks the cash machine");
+			out.add("W- Withdraws that amount from the cash machine (e.g. W $145");
+			out.add("O- Displays the number of bills in that denomination present in the cash machine (e.g. I $20 $10 $1)");
+			out.add("Q - Quits the application");*/
 			String in = input.nextLine();
 			String[] inx = in.split(" " + "\\$");
 			if (inx[0].equals("R")) {
 				atm.R();
-				output.println("Reset");
+				out.add("Reset");
 			} else if (inx[0].equals("W") && (atm.isInteger(inx[1]))) {
 				if (Integer.parseInt(inx[1]) > 0) {
-					output.println(atm.W(Integer.parseInt(inx[1])));
+					out.add(atm.W(Integer.parseInt(inx[1])));
 				} else {
-					output.println("Did not understand");
+					out.add("Did not understand");
 				}
 			} else if (inx[0].equals("I")) {
 				int y = 0;
@@ -49,17 +52,21 @@ public class testATM {
 					} else if (inx[i].equals("1000")) {
 						y = 6;
 					} else {
-						output.println("Did not understand");
+						out.add("Did not understand");
 						break OuterLoop;
 					}
-					output.println(atm.I(y));
+					out.add(atm.I(y));
 				}
 			} else if (inx[0].equals("Q")) {
-				output.println("Quit the program");
+				out.add("Quit the program");
 				tru = false;
 			} else {
-				output.println("Did not understand");
+				out.add("Did not understand");
 			}
+		}
+		PrintWriter output = new PrintWriter(new FileOutputStream(outputFile));
+		for (String outP : out) {
+			output.println(outP);
 		}
 		output.close();
 	}
